819bca3f806ad038765876fc85e38f8e725d9af1
hashimoto <tomorow326@gmail.com>
2021-01-02 15:36:15 +0900

    Add automatic resize function to editor

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,11 +1,26 @@
 import { NextPage } from 'next'
+import React from 'react'
+import autosize from 'autosize'
+
 import styles from './index.module.css'
 
 const PostPage: NextPage = () => {
+  const ref = React.useRef<HTMLTextAreaElement>(null)
+
+  React.useEffect(() => {
+    if (ref.current) {
+      autosize(ref.current)
+    }
+  }, [])
+
   return (
     <div className={styles.editContent}>
       <input type="text" className={styles.subject} placeholder={'タイトル'} />
-      <textarea className={styles.editor} placeholder={'本文を書きましょう'} />
+      <textarea 
+        className={styles.editor} 
+        placeholder={'本文を書きましょう'} 
+        ref={ref}
+      />
     </div>
   )
 }
